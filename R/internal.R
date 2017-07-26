cbbba26c1e1b869095ee814c574b93aaf0058b22
Dennis Amelunxen <damelunx@gmail.com>
2017-07-26 14:37:03 +0800

    some renaming

--- R/internal.R
+++ R/internal.R
@@ -1,15 +1,15 @@
 
 # adjust tolerance level for some numeric computations
 #
-.conivol_adj_tol <- 10*.Machine$double.eps
+.adj_tol <- 10*.Machine$double.eps
 
 
 
 # just a small function to use in the other function for testing whether an
 # input is a genuine vector of probabilities
 #
-.conivol_test_vector <- function(v){
-    if (any(v<0 & sapply(v, function(t){!isTRUE(all.equal(t,0,tolerance=conivol:::.conivol_adj_tol))}) )
+.test_vector <- function(v){
+    if (any(v<0 & sapply(v, function(t){!isTRUE(all.equal(t,0,tolerance=conivol:::.adj_tol))}) )
         | any(v>1) | !isTRUE(all.equal(sum(v),1)))
         stop("Vector v must be a discrete probability distribution.")
 }
